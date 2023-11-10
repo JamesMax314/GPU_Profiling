cf50dd72e43828a519c3d7b81645e5516bed38c1
James Maxwell <james.l.maxwell@durham.ac.uk>
2023-11-10 15:14:34 +0000

    Added black box

--- finite_fields/finite_field.hpp
+++ finite_fields/finite_field.hpp
@@ -1,24 +1,55 @@
+#include <vector>
+#include <cmath>
+
 template<typename T> class finite_field{
 
 	public:
-	finite_field(T prime, T value) : value(value), prime(prime) {}
+	finite_field(T prime, T value) : value(value%prime), prime(prime) {}
 
-	template<typename U> finite_field<T> operator+(U rvalue){return finite_field(prime, (value + rvalue)%prime);}
+	template<typename U> U getVal(U rvalue) {return rvalue;};
+	template<typename U> U getVal(finite_field<U> rvalue) {return rvalue.value;};
+
+	template<typename U> finite_field<T> operator+(U rvalue){return finite_field(prime, (value + getVal(rvalue)));}
+	template<typename U> finite_field<T> operator-(U rvalue){return finite_field(prime, (value - getVal(rvalue)));}
+	template<typename U> finite_field<T> operator*(U rvalue){return finite_field(prime, (value * getVal(rvalue)));}
+	template<typename U> finite_field<T> operator/(U rvalue){return finite_field(prime, (value / getVal(rvalue)));}
+
+	template<typename U> finite_field<T> pow(U rvalue){
+		T newVal = value;
+		for (int i=0; i<getVal(rvalue)-1; i++) {
+			newVal *= value;
+		}
+		return finite_field(prime, newVal);
+	}
 
 	template<typename U> finite_field<T>& operator+=(U rvalue){
-			this->value = (value + rvalue)%prime;
-			return *this;
+		this->value = (value + getVal(rvalue))%prime;
+		return *this;
 	}
-
-	template<typename U> finite_field<T> operator-(U rvalue){return finite_field(prime, (value - rvalue)%prime);}
 
 	template<typename U> finite_field<T>& operator-=(U rvalue){
-			this->value = (value - rvalue)%prime;
-			return *this;
+		this->value = (value - getVal(rvalue))%prime;
+		return *this;
 	}
 
-	template<typename U> finite_field<T> operator=(U rvalue){value = (rvalue)%prime;}
+	template<typename U> finite_field<T>& operator*=(U rvalue){
+		this->value = (value * getVal(rvalue))%prime;
+		return *this;
+	}
+
+	template<typename U> finite_field<T> operator=(U rvalue){value = (getVal(rvalue))%prime;}
 
 	T value, prime;
 };
 
+
+class black_box {
+	private:
+
+	public:
+	black_box() {};
+
+	// Called by cuda transform to do operation at many different values
+	template<typename T>
+	finite_field<T> operator()(std::vector<finite_field<T>> rvalues) {return rvalues[0].pow(7) + rvalues[1].pow(8);};
+};
--- finite_fields/main.cpp
+++ finite_fields/main.cpp
@@ -1,14 +1,22 @@
 #include <cstdio>
+#include <algorithm> 
 #include "finite_field.hpp"
 
-finite_field<int> ff(13, 2);
+finite_field<int> ff(100, 2);
+finite_field<int> ff2(13, 12);
+black_box bb;
 
 int main(){
 
-	for(int i=0; i<100; i++){
-		ff += 1;
-		printf("%i \n", ff.value);
-	}
+	// for(int i=0; i<100; i++){
+	// 	ff += 1;
+	// 	printf("%i \n", ff.value);
+	// }
+	std::vector<finite_field<int>> vec;
+	vec.emplace_back(ff);
+	vec.emplace_back(ff2);
+
+	printf("%i \n", bb(vec).value);
 	
 	return 0;
 }
