e8d36af4d073e00d0190ec19848a18c22e59b19b
Jack Franklin <jfranklin@ip3-gpu2.phyip3.dur.ac.uk>
2023-11-20 15:31:55 +0000

    Working runtime unit test

--- finite_fields/tests/subtraction.cpp
+++ finite_fields/tests/subtraction.cpp
@@ -1,7 +1,19 @@
+#include <string>
+#include <iostream>
+
 #include "../finite_field.hpp"
 
-finite_field<int> a(1388171, 1000);
-finite_field<int> b(1388171, 120000);
+constexpr int subtest(){
 
-static_assert((a-b) == finite_field<int>(1388171, 1269171));
+    finite_field<int> a(1388171, 1000);
+    finite_field<int> b(1388171, 120000);
 
+    int result = (a-b).value();
+
+    return result;
+}
+ 
+int main(){
+    std::cout<<"Expected: 1269171, Got: "<<subtest()<<std::endl;
+    return 0;
+}
